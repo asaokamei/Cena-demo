ebc639887fd2bdf46a8bed8f114a8fd4525b0a21
Asao <kamei@workspot.jp>
2014-02-03 15:42:30 +0900

    separate Post class to Post and PostDto, for doctrine2 part and its responsibilities.

--- src/Demo/Models/Post.php
+++ src/Demo/Models/Post.php
@@ -1,6 +1,8 @@
 <?php
 namespace Demo\Models;
 
+use Michelf\MarkdownExtra;
+
 /**
  * Class Post
  * @package Demo\Models
@@ -9,115 +11,15 @@ namespace Demo\Models;
  * @Table(name="post")
  * @HasLifecycleCallbacks
  */
-class Post
+class Post extends PostDto
 {
     /**
-     * @var int
-     * @id @GeneratedValue
-     * @Column(type="integer")
-     */
-    protected $post_id;
-
-    /**
-     * @var string
-     * @Column(type="string")
-     */
-    protected $title;
-
-    /**
-     * @var string
-     * @Column(type="string")
-     */
-    protected $content;
-
-    /**
-     * @var \DateTime
-     * @Column(type="datetime")
-     */
-    protected $createdAt;
-
-    /**
-     * @var \DateTime
-     * @Column(type="datetime")
-     */
-    protected $updatedAt;
-
-    /**
-     * @var
-     * @OneToMany( targetEntity="Demo\Models\Comment", mappedBy="post" )
-     * @JoinColumn( name="comment_id", referencedColumnName="comment_id" )
-     */
-    protected $comments;
-
-    /**
-     * @PrePersist
-     */
-    public function setCreatedAt()
-    {
-        $this->createdAt = new \DateTime('now');
-    }
-
-    /**
-     * @PreUpdate
-     */
-    public function setUpdatedAt()
-    {
-        $this->updatedAt = new \DateTime('now');
-    }
-
-    /**
-     * @return int
-     */
-    public function getPostId()
-    {
-        return $this->post_id;
-    }
-
-    /**
+     * returns html from markdown text.
+     *
      * @return string
      */
-    public function getTitle()
+    public function getContentHtml()
     {
-        return $this->title;
-    }
-
-    /**
-     * @param string $title
-     */
-    public function setTitle( $title )
-    {
-        $this->title = $title;
-    }
-
-    /**
-     * @return string
-     */
-    public function getContent()
-    {
-        return $this->content;
-    }
-
-    /**
-     * @param string $content
-     */
-    public function setContent( $content )
-    {
-        $this->content = $content;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getCreatedAt()
-    {
-        return $this->createdAt;
-    }
-
-    /**
-     * @return \DateTime
-     */
-    public function getUpdatedAt()
-    {
-        return $this->updatedAt;
+        return MarkdownExtra::defaultTransform( $this->content );
     }
 }
---
+++ src/Demo/Models/PostDto.php
@@ -0,0 +1,120 @@
+<?php
+namespace Demo\Models;
+
+/**
+ * Class PostDto
+ * @package Demo\Models
+ *
+ */
+class PostDto
+{
+    /**
+     * @var int
+     * @id @GeneratedValue
+     * @Column(type="integer")
+     */
+    protected $post_id;
+
+    /**
+     * @var string
+     * @Column(type="string")
+     */
+    protected $title;
+
+    /**
+     * @var string
+     * @Column(type="string")
+     */
+    protected $content;
+
+    /**
+     * @var \DateTime
+     * @Column(type="datetime")
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     * @Column(type="datetime")
+     */
+    protected $updatedAt;
+
+    /**
+     * @var
+     * @OneToMany( targetEntity="Demo\Models\Comment", mappedBy="post" )
+     * @JoinColumn( name="comment_id", referencedColumnName="comment_id" )
+     */
+    protected $comments;
+
+    /**
+     * @PrePersist
+     */
+    public function setCreatedAt()
+    {
+        $this->createdAt = new \DateTime('now');
+    }
+
+    /**
+     * @PreUpdate
+     */
+    public function setUpdatedAt()
+    {
+        $this->updatedAt = new \DateTime('now');
+    }
+
+    /**
+     * @return int
+     */
+    public function getPostId()
+    {
+        return $this->post_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle( $title )
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent( $content )
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+}
