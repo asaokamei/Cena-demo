600e30bb6866333c17359ecb1eaf1919fa8ee258
Asao <kamei@workspot.jp>
2014-03-09 19:23:49 +0900

    changed interface; accept $input using setInput.

--- src/Demo/Resources/CommentValidator.php
+++ src/Demo/Resources/CommentValidator.php
@@ -1,61 +1,8 @@
 <?php
 namespace Demo\Resources;
 
-use Cena\Cena\Validation\ValidatorInterface;
+use Cena\Cena\Validation\DumbValidatorAbstract;
 
-class CommentValidator implements ValidatorInterface
+class CommentValidator extends DumbValidatorAbstract
 {
-    protected $entity;
-    
-    /**
-     * set entity object.
-     *
-     * @param object $entity
-     * @return mixed
-     */
-    public function setEntity( $entity )
-    {
-        $this->entity = $entity;
-    }
-
-    /**
-     * validate the input.
-     *
-     * @param array $input
-     * @return array
-     */
-    public function validate( $input )
-    {
-        return $input;
-    }
-
-    /**
-     * verify that the entity is valid.
-     *
-     * @return bool
-     */
-    public function verify()
-    {
-        return true;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isValid()
-    {
-        return true;
-    }
-
-    /**
-     * get the errors as array.
-     * returns array as
-     *     [ 'key' => 'error message' ]
-     *
-     * @return array
-     */
-    public function getErrors()
-    {
-        return array();
-    }
 }
